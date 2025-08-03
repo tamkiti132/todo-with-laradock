c055044cbddfe1d12373df9c2130a97fb7d06823
tamkiti132 <harada.a.0907@gmail.com>
2025-08-04 04:28:19 +0900

    chore: tailwindcssとviteを動作させるための設定

--- todo-with-laradock/vite.config.js
+++ todo-with-laradock/vite.config.js
@@ -10,4 +10,16 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+    //  以下は、Docker環境でviteのHMRを動作させるための設定。
+    //  以下のページを参考にした。
+    //  https://nnahito.com/articles/107#header-0
+    server: {
+        host: true,
+        hmr: {
+            host: 'localhost',    // Docker環境のhostを設定
+        },
+        watch: {
+            usePolling: true,    // WindowsのWSL2やDocker環境でも動くように設定
+        }
+    }
 });
